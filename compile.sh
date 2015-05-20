201d9a40764d3bf635852d667fbeda1fd02174f3
Venkat K <venkat@mahaswami.com>
2015-05-20 18:18:34 +0530

    Simple example

---
+++ compile.sh
@@ -0,0 +1,1 @@
+/Library/Java/JavaVirtualMachines/jdk1.7.0_55.jdk/Contents/Home/bin/javac -cp  "/Users/venkat/workspace/drools-distribution-6.2.0.Final/binaries/*" org/drools/examples/helloworld/HelloWorldExample.java
---
+++ run.sh
@@ -0,0 +1,1 @@
+java -cp "/Users/venkat/workspace/drools-distribution-6.2.0.Final/binaries/*:slf4j-simple-1.7.12.jar:." org.drools.examples.helloworld.HelloWorldExample
